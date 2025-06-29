34508b3bdb6bec7d473dc09df1253a188afba7b2
Ridham-patel19 <patelridham737@gmail.com>
2025-06-29 22:32:25 +0530

    change fetch url in browserjs

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -25,7 +25,7 @@ weatherform.addEventListener('submit', (e) => {
   msg2.textContent = '';
   msg3.textContent = '';
 
-  fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+  fetch(`/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
       outputElement.classList.remove('loading');
        outputElement.classList.add('outputdata1')
